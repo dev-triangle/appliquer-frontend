eb21dc8b93717ecb671a7bbcedba055453a5f06f
Adithya Ajith <4adhi007@gmail.com>
2022-08-10 01:11:43 +0530

    signup axios

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import axios from 'axios'
+import baseUrl from '../../utils/urls'
+import { useState } from 'react'
 
 import '../Login/Loginsignup.css'
 
 function Signup() {
+    const[email, setEmail]=useState("");
+    const[username,setUsername]=useState("");
+    const[password,setPassword]=useState("");
+
+    handlesubmit=async()=>{
+        await axios.post(`${baseUrl}/register/`,{username})
+    }
+
+
   return (
     <div className='login1_container'>
     <section className='login_section'>
@@ -20,15 +31,15 @@ function Signup() {
             <div className="login_container">
             <div class="login_form">
                 <h2>SignUp Form</h2>
-                <form>
+                <form onSubmit={handlesubmit()}>
                 <div class="login_inputBox">
-                        <input type="text" placeholder="Email"/>
+                        <input type="text" value={email} placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
                     </div>
                     <div class="login_inputBox">
-                        <input type="text" placeholder="Username"/>
+                        <input type="text" value={username} placeholder="Username" onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
                     <div class="login_inputBox">
-                        <input type="password" placeholder="Password"/>
+                        <input type="password" value={password} placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
                     </div>
                     <div class="login_inputBox">
                         <input type="submit" value="SignUp"/>
