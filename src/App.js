77466166bd567e38ec55722137b11333ab34d6c5
Adithya Ajith <4adhi007@gmail.com>
2022-08-09 22:55:50 +0530

    m

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from './utils/ScrollToTop';
 import Home from './pages/Home/Home';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
 function App() {
   return (
     <div className="App">
@@ -10,6 +12,8 @@ function App() {
       <ScrollToTop/>
       <Routes>
         <Route path='' element={<Home/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+import '../Login/Loginsignup.css'
+
+function Signup() {
+  return (
+    <div className='login1_container'>
+    <section className='login_section'>
+        <div class="color"></div>
+        <div class="color"></div>
+        <div class="color"></div>
+        <div class="login_box">
+        <div class="square" style={{'--i':0}}></div>
+        <div class="square" style={{'--i':1}}></div>
+        <div class="square" style={{'--i':2}}></div>
+        <div class="square" style={{'--i':3}}></div>
+        <div class="square" style={{'--i':4}}></div>
+            </div>
+            <div className="login_container">
+            <div class="login_form">
+                <h2>Login Form</h2>
+                <form>
+                    <div class="login_inputBox">
+                        <input type="text" placeholder="Username"/>
+                    </div>
+                    <div class="login_inputBox">
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                    <div class="login_inputBox">
+                        <input type="submit" value="Login"/>
+                    </div>
+                    <p class="forget">Forgot Password ? <a href="#">Click Here</a></p>
+                    <p class="forget">Don't have an account ? <a href="#">Sign Up</a></p>
+                </form>
+            </div>
+        </div>
+    </section>
+   
+    </div>
+  )
+}
+
+export default Signup
